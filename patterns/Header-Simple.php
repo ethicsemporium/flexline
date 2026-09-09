de010f8ffd948b85862a9d6c18bddfd7fce8f71d
ethicsemporium <ethicsemporiumllc@gmail.com>
2026-09-08 20:09:56 -0400

    Create Header-Simple.php

---
+++ patterns/Header-Simple.php
@@ -0,0 +1,25 @@
+<!-- wp:buttons {"className":"","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-text-link"} -->
+<div class="wp-block-button is-style-text-link"><a class="wp-block-button__link wp-element-button">Home</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-text-link"} -->
+<div class="wp-block-button is-style-text-link"><a class="wp-block-button__link wp-element-button">Portfolio</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-text-link"} -->
+<div class="wp-block-button is-style-text-link"><a class="wp-block-button__link wp-element-button">Services</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-text-link"} -->
+<div class="wp-block-button is-style-text-link"><a class="wp-block-button__link wp-element-button">About</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-text-link"} -->
+<div class="wp-block-button is-style-text-link"><a class="wp-block-button__link wp-element-button">Book Now</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-text-link"} -->
+<div class="wp-block-button is-style-text-link"><a class="wp-block-button__link wp-element-button">FAQ</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
